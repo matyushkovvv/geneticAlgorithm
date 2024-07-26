582dc65d5be59cd484364e49d6a849029bcd630e
matyushkovvv <anmatyushkov@yandex.ru>
2024-07-26 10:18:20 +0300

    Update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
-from BoardView import Board
+from ViewDelegate import GameDelegate
+from Entity import Entity
+from EntityType import EntityType
 
 if __name__ == '__main__':
-    board = Board(100, 100)
-    board.mainloop()
+
+    entities = [
+        Entity(5, 5, EntityType.HYBRID),
+        Entity(11, 56, EntityType.PLANT),
+        Entity(81, 3, EntityType.PREDATOR)
+    ]
+
+    GameDelegate.init(100, 100)
+    GameDelegate.draw_board()
+    GameDelegate.update_entities(entities)
+
+    GameDelegate.mainloop()
