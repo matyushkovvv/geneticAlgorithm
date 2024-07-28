9f062e2a27e8042d7aa24c4af7c11142419227bb
matyushkovvv <anmatyushkov@yandex.ru>
2024-07-28 14:14:46 +0300

    Rename Delegate to ViewDelegate

--- src/ViewDelegate.py
+++ src/ViewDelegate.py
@@ -1,11 +1,11 @@
 from BoardView import Board
 
 
-class Delegate:
+class ViewDelegate:
     @staticmethod
     def init(cols, rows):
-        Delegate.board = Board(cols, rows)
+        ViewDelegate.board = Board(cols, rows)
 
     @staticmethod
     def update_entities(entities_list):
-        Delegate.board.update_entities(entities_list)
+        ViewDelegate.board.update_entities(entities_list)
